ea80490d4d85f95a119594fdfb8c433afd5a2186
Sami Kojo-Fry <sami.kojo@tuni.fi>
2022-02-18 12:16:29 +0200

    Add: Touchscreen controls

--- Assets/Code/CharacterControl.cs
+++ Assets/Code/CharacterControl.cs
@@ -9,6 +9,12 @@ namespace PeliprojektiExamples
 	[RequireComponent(typeof(InputProcessor))]
 	public class CharacterControl : MonoBehaviour
 	{
+		public enum ControlState
+		{
+			GamePad,
+			Touch
+		}
+
 		[SerializeField]
 		private float velocity = 1;
 
@@ -17,6 +23,12 @@ namespace PeliprojektiExamples
 		private new SpriteRenderer renderer;
 
 		private Vector2 moveInput;
+
+		private Vector2 touchPosition;
+
+		private Vector2 targetPosition;
+
+		private ControlState controlState = ControlState.GamePad;
 
 		private void Awake()
 		{
@@ -62,15 +74,72 @@ namespace PeliprojektiExamples
 
 		private void MoveCharacter()
 		{
-			Vector2 movement = moveInput * Time.deltaTime * velocity;
-			// transform property allows us to read and manipulate GameObject's position
-			// in the game world.
-			transform.Translate(movement);
+			switch (controlState)
+			{
+				case ControlState.GamePad:
+					Vector2 movement = moveInput * Time.deltaTime * velocity;
+					// transform property allows us to read and manipulate GameObject's position
+					// in the game world.
+					transform.Translate(movement);
+					break;
+				case ControlState.Touch:
+					// Koska Vector2:sta ei voi vähentää Vector3:a, pitää suorittaa tyyppimuunnos
+					Vector3 travel = (Vector3)targetPosition - transform.position;
+
+					// Normalisointi muuntaa vektorin pituuden yhdeksi
+					Vector3 frameMovement = travel.normalized * velocity * Time.deltaTime;
+
+					// magnitude palauttaa vektorin pituuden. Tässä vektorin pituus kuvaa
+					// jäljellä olevaa matkaa
+					float distance = travel.magnitude;
+
+					if (frameMovement.magnitude < distance)
+					{
+						// Matkaa on vielä jäljellä, kuljetaan kohti kohdepistettä
+						transform.Translate(frameMovement);
+					}
+					else
+					{
+						// Päämäärä saavutettu
+						transform.position = targetPosition;
+						moveInput = Vector2.zero;
+					}
+
+					break;
+			}
 		}
 
 		private void OnMove(InputAction.CallbackContext callbackContext)
 		{
+			controlState = ControlState.GamePad;
 			moveInput = callbackContext.ReadValue<Vector2>();
+		}
+
+		private void OnTap(InputAction.CallbackContext context)
+		{
+			Debug.Log("Tap!");
+			Debug.Log(context.phase);
+		}
+
+		private void OnTouchPosition(InputAction.CallbackContext context)
+		{
+			controlState = ControlState.Touch;
+
+			this.touchPosition = context.ReadValue<Vector2>();
+
+			// Muunnetaan 2D koordinaatti 3D-koordinaatistoon
+			Vector3 screenCoordinate = new Vector3(touchPosition.x, touchPosition.y, 0);
+
+			// Muunnetaan näytön koordinaatti pelimaailman koordinaatistoon
+			Vector3 worldCoordinate = Camera.main.ScreenToWorldPoint(screenCoordinate);
+
+			// Muunnetaan maailman koordinaatti 2D-koordinaatistoon. HUOM! implisiittinen
+			// tyyppimuunnos Vector3 -> Vector2
+			targetPosition = worldCoordinate;
+
+			// Päivitetään myös moveInput-vektoria, koska animaattorin parametrit asetetaan sen
+			// perusteella
+			moveInput = (targetPosition - (Vector2)transform.position).normalized;
 		}
 	}
 }
